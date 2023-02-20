e5d5c677af12309a6a73c3428841a21fc14e5b8f
narumifumiya <ec2-user@ip-172-31-42-249.ap-northeast-1.compute.internal>
2023-02-20 11:14:29 +0000

    [製作中]2/20

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: "homes#top"
+  devise_for :users
+  
+  resources :books, only: [:new, :create, :index, :show]
+  
   get 'home/about' => 'homes#about', as: 'about'
   
 end

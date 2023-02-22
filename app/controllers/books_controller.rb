af264950ffad86d00fd6511593557eab1a2e43ab
narumifumiya <ec2-user@ip-172-31-42-249.ap-northeast-1.compute.internal>
2023-02-22 16:34:42 +0000

    [製作中]2/23

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @user = current_user
@@ -38,8 +39,13 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   private
